944b1db726f763a976c675816ec22469c87e76a5
Alejandro <alejandro199898@gmail.com>
2021-01-06 22:05:54 -0800

    add plant svg

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <img src="/plant.svg" alt="me" width="64" height="64" />
       <div>E-commerce</div>
     </div>
   )

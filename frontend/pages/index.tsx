d615c154b5eb25e258d9365936d94cf824f84326
Alejandro <alejandro199898@gmail.com>
2021-01-22 19:22:45 -0800

    remove css & html clutter

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,22 +9,10 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* <Nav></Nav> */}
       <NavigationItems />
-      
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }

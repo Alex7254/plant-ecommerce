b4cfbb4bc2a4c295b51900672903f2503d09747a
Alejandro <alejandro199898@gmail.com>
2021-01-30 17:29:58 -0800

    add background image to hero section

--- frontend/Components/homepage/HeroSection.tsx
+++ frontend/Components/homepage/HeroSection.tsx
@@ -3,18 +3,27 @@ import styled from 'styled-components'
 import WaveBg from '../../public/svg/HeaderWave.svg'
 
 const HeroArea = styled.div`
-   background: url(${WaveBg}) no-repeat center center fixed; 
+   background: url(${WaveBg}) no-repeat center center; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
   width: 100%;
-  background-color: red;
+  height: 80vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 150px;
+  /* background-color: #946464; */
 `;
 
 const HeroSection = () => {
   return (
     <HeroArea>
+      <div>
+        <h1>Cat Farm Succulents</h1>
+        <h3>Indoor and outdoor succulents.</h3>
+      </div>
     </HeroArea>
   )
 }

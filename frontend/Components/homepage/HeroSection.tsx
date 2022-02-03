cce64209459e069c4d5f38cb8e775dbc9ef723a6
Alejandro <alejandro199898@gmail.com>
2022-02-03 14:56:54 -0800

    updates to next.js and UI

--- frontend/Components/homepage/HeroSection.tsx
+++ frontend/Components/homepage/HeroSection.tsx
@@ -1,54 +1,65 @@
-import styled from 'styled-components'
-import Image from 'next/image'
+import styled from "styled-components";
+import Image from "next/image";
 
-import WaveBg from '../../public/svg/HeaderWave.svg'
-import Button from '../buttons/Button'
+import WaveBg from "../../public/svg/HeaderWave.svg";
+import Button from "../buttons/Button";
 
-const HeroArea = styled.div`
-  background: url(${WaveBg}) no-repeat center center; 
+const HeroArea = styled.section`
+  background: url(${WaveBg}) no-repeat center center;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
   width: 100%;
   height: 90vh;
+  
+  /* padding: 0 150px; */
+`;
+
+const HeroMaxWidth = styled.div`
+  /* background-color: red; */
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  max-width: 120rem;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 0 150px;
-  /* background-color:red; */
 `;
 
-const HeroText = styled.div`
-cursor: default;
-/* background-color:red; */
-  color: #333300;
+const HeroHeader = styled.div`
+  cursor: default;
+  color: var(--dark-green);
   > h1 {
     font-size: 3.2rem;
   }
   > h3 {
     font-weight: 300;
-    color: #666633;
+    color: var(--light-green);
   }
   > button {
     margin-top: 45px;
   }
-`
+`;
 const HeroSection = () => {
   return (
     <HeroArea>
-      <HeroText>
-        <h1>Farm Cat Succulents</h1>
-        <h3>Indoor and outdoor succulents.</h3>
-        <Button buttonText="Our Blog" />
-      </HeroText>
-      <Image src="/svg/plants.svg"
-        alt="Picture of the author"
-        width={500}
-        height={500}></Image>
+      <HeroMaxWidth>
+        <HeroHeader>
+          <h1>Farm Cat Succulents</h1>
+          <h3>Indoor and outdoor succulents.</h3>
+          <Button buttonText="Our Store" />
+        </HeroHeader>
+        <Image
+          src="/svg/plants.svg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        ></Image>
+      </HeroMaxWidth>
     </HeroArea>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,38 +1,40 @@
-import Head from 'next/head'
-import { createGlobalStyle } from 'styled-components'
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
 
-import SectionWrapper from '../hoc/layout/SectionWrapper'
-import NavigationLinks from '../components/navigation/NavigationLinks'
-import HeroSection from '../components/homepage/HeroSection'
-import SectionTitle from '../components/common/SectionTitle'
+import SectionWrapper from "../hoc/layout/SectionWrapper";
+import NavigationLinks from "../components/navigation/NavigationLinks";
+import HeroSection from "../components/homepage/HeroSection";
+import SectionTitle from "../components/common/SectionTitle";
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --dark-green: #333300;
+    --light-green: #666633;
+  }
+  body {
+    font-family: "Poppins", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
+      Droid Sans, Helvetica Neue, sans-serif;
+  }
 
-body {
-  font-family: "Poppins", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
-    Droid Sans, Helvetica Neue, sans-serif;
-}
+  a {
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: 300;
+    letter-spacing: 0.5px;
+  }
 
-a {
-  text-decoration: none;
-  font-size: 14px;
-  font-weight: 300;
-  letter-spacing: 0.5px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-button {
-  border: none;
-  font-family: "Poppins";
-  cursor: pointer;
-}
-
-`
+  button {
+    border: none;
+    font-family: "Poppins";
+    cursor: pointer;
+  }
+`;
 
 export default function Home() {
   return (
@@ -40,16 +42,18 @@ export default function Home() {
       <GlobalStyle />
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
         <title>Cat Farm Succulents</title>
       </Head>
 
-      {/* <Nav></Nav> */}
       <NavigationLinks />
       <HeroSection />
       <SectionWrapper>
-        <SectionTitle >Featured Plants</SectionTitle>
+        <SectionTitle>Featured Plants</SectionTitle>
       </SectionWrapper>
     </div>
-  )
+  );
 }

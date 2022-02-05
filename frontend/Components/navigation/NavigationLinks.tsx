943147116e0f84f0488f4d0c0ed1cf89585c75c7
Alejandro <alejandro199898@gmail.com>
2022-02-04 17:39:06 -0800

    edit nav max-width

--- frontend/Components/navigation/NavigationLinks.tsx
+++ frontend/Components/navigation/NavigationLinks.tsx
@@ -1,40 +1,45 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import NavigationLink from './NavigationLink';
-import CartButton from './CartButton';
-import CactusLogo from './Logo';
+import NavigationLink from "./NavigationLink";
+import CartButton from "./CartButton";
+import CactusLogo from "./Logo";
 
 const StyledUl = styled.ul`
-    list-style-type: none;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 const StyledNav = styled.nav`
-    width: 100%;
-    position: fixed;
-    padding: 20px 150px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  max-width: 120rem;
+  width: 100%;
+  margin: 0 auto;
+  z-index: 2;
+  position: fixed;
+  left: 0;
+  right: 0;
+  padding: 20px 150px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-const cartButton = styled.a
+const cartButton = styled.a;
 
 const navigationItems = () => (
-    <StyledNav>
-        <CactusLogo />
-        <StyledUl>
-            <NavigationLink link="/">Home</NavigationLink>
-            <NavigationLink link="/projects">Projects</NavigationLink>
-            <NavigationLink link="/about">About</NavigationLink>
-            <NavigationLink link="/experience">Experience</NavigationLink>
-            <CartButton />
-        </StyledUl>
-    </StyledNav>
+  <StyledNav>
+    <CactusLogo />
+    <StyledUl>
+      <NavigationLink link="/">Home</NavigationLink>
+      <NavigationLink link="/projects">Projects</NavigationLink>
+      <NavigationLink link="/about">About</NavigationLink>
+      <NavigationLink link="/experience">Experience</NavigationLink>
+      <CartButton />
+    </StyledUl>
+  </StyledNav>
 );
 
 export default navigationItems;

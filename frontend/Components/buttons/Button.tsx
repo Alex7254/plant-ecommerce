e5f2be3d782769ea6d7731d0e8c34b17cf9016bd
Alejandro <alejandro199898@gmail.com>
2022-02-07 16:50:04 -0800

    format code

--- frontend/Components/buttons/Button.tsx
+++ frontend/Components/buttons/Button.tsx
@@ -1,4 +1,4 @@
-import styled, {css} from 'styled-components'
+import styled, { css } from "styled-components";
 
 const StyledButton = styled.button`
   color: #fff;
@@ -8,24 +8,39 @@ const StyledButton = styled.button`
   font-weight: 300;
   border: none;
   border-radius: 0.313rem;
-  background: rgb(221,161,94);
-  background: -moz-linear-gradient(90deg, rgba(221,161,94,1) 0%, rgba(188,108,37,1) 100%);
-  background: -webkit-linear-gradient(90deg, rgba(221,161,94,1) 0%, rgba(188,108,37,1) 100%);
-  background: linear-gradient(90deg, rgba(221,161,94,1) 0%, rgba(188,108,37,1) 100%);
+  background: rgb(221, 161, 94);
+  background: -moz-linear-gradient(
+    90deg,
+    rgba(221, 161, 94, 1) 0%,
+    rgba(188, 108, 37, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    90deg,
+    rgba(221, 161, 94, 1) 0%,
+    rgba(188, 108, 37, 1) 100%
+  );
+  background: linear-gradient(
+    90deg,
+    rgba(221, 161, 94, 1) 0%,
+    rgba(188, 108, 37, 1) 100%
+  );
   filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#dda15e",endColorstr="#bc6c25",GradientType=1);
-
-  ${props => props.btnFlat && css`
-    background: none;
-    color: #333300;
-  `}
-`
+  moz-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  -webkit-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+  ${(props) =>
+    props.btnFlat &&
+    css`
+      background: none;
+      color: #333300;
+    `}
+`;
 
 const Button = (props) => {
   return (
-    <StyledButton btnFlat={props.flatStyle}>
-      {props.buttonText}
-    </StyledButton>
-  )
-}
+    <StyledButton btnFlat={props.flatStyle}>{props.buttonText}</StyledButton>
+  );
+};
 
-export default Button
+export default Button;

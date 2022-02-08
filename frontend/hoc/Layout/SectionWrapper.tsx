b47d72b780f94ff3fa07503a8d27f5da6c979b9b
Alejandro <alejandro199898@gmail.com>
2022-02-07 16:18:42 -0800

    edit wrapper width

--- frontend/hoc/Layout/SectionWrapper.tsx
+++ frontend/hoc/Layout/SectionWrapper.tsx
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div ` 
   /* height: 40rem; */
-  max-width: 115rem;
-  padding: 8rem 4rem;
+  max-width: 120rem;
+  padding: 8rem 0;
   margin: 0 auto;
   /* background-color: red; */
 `

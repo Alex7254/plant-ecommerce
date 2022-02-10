e155d940184c2d11c2c6134f794366bcdd85b03d
Alejandro <alejandro199898@gmail.com>
2022-02-10 11:13:15 -0800

    rename itemCard file to productCard

---
+++ frontend/Components/common/ProductCard.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+  background-color: var(--beige);
+  height: 11.625rem;
+  width: 15.813rem;
+  border-radius: 1rem;
+`
+
+const ProductCard = (props) => {
+  return (
+    <CardContainer></CardContainer>
+  )
+}
+
+export default ProductCard
--- frontend/Components/homepage/featuredplants/index.js.tsx
+++ frontend/Components/homepage/featuredplants/index.js.tsx
@@ -1,12 +1,14 @@
 import SectionWrapper from "../../../hoc/Layout/SectionWrapper";
 import SectionTitle from "../../../Components/common/SectionTitle";
+import ProductCard from "../../common/ProductCard";
 
 const FeaturedPlants = () => {
   return (
     <SectionWrapper>
       <SectionTitle>Featured Plants</SectionTitle>
+      <ProductCard />
     </SectionWrapper>
   );
 };
 
-export default FeaturedPlants
+export default FeaturedPlants;

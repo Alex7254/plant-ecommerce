5122cb03720f8d4cc1d46b0013f9957626b928a3
Alejandro <alejandro199898@gmail.com>
2022-02-10 13:06:42 -0800

    add product image

--- frontend/Components/common/ProductCard.tsx
+++ frontend/Components/common/ProductCard.tsx
@@ -2,14 +2,24 @@ import styled from 'styled-components'
 
 const CardContainer = styled.div`
   background-color: var(--beige);
-  height: 11.625rem;
+  height: 22rem;
   width: 15.813rem;
   border-radius: 1rem;
+  /* position: relative; */
+`
+
+const ProductImage = styled.img `
+  height: 13rem;
+  width: 100%;
+  /* position absolute; */
+  /* top: -7.3rem; */
 `
 
 const ProductCard = (props) => {
   return (
-    <CardContainer></CardContainer>
+    <CardContainer>
+      <ProductImage src={props.imgUrl} alt={props.imgAlt}/>
+    </CardContainer>
   )
 }
 

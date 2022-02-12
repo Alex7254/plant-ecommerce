ff31bed10c6c139d4359a3e610ff84ff61c9586e
Alejandro <alejandro199898@gmail.com>
2022-02-12 12:25:15 -0800

    add product name to product card

--- frontend/Components/common/ProductCard.tsx
+++ frontend/Components/common/ProductCard.tsx
@@ -1,30 +1,43 @@
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-  height: 22rem;
+  height: 21.5rem;
   width: 18rem;
   position: relative;
 `;
 
 const ProductImage = styled.img`
-  max-height: 14.2rem;
+  max-height: 13.5rem;
+  display: block;
+  margin: 0 auto;
 `;
 
 const ProductDetails = styled.div`
-  height: 11rem;
+  height: 13rem;
   width: 100%;
   position: absolute;
-  top: 11rem;
+  top: 8.5rem;
   z-index: -1;
   background-color: var(--beige);
   border-radius: 1rem;
+`;
+
+const ProductName = styled.p`
+  font-size: 1.2rem;
+  color: var(--dark-green);
+  font-weight: 500;
+  position: absolute;
+  top: 7rem;
+  left: 1.8rem;
 `;
 
 const ProductCard = (props) => {
   return (
     <CardContainer>
       <ProductImage src={props.imgUrl} alt={props.imgAlt} />
-      <ProductDetails />
+      <ProductDetails>
+        <ProductName>{props.productName}</ProductName>
+      </ProductDetails>
     </CardContainer>
   );
 };

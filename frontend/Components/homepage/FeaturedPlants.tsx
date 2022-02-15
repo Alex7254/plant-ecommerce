ed3c27ad7de7002b681a288a0179d2f6899ba164
Alejandro <alejandro199898@gmail.com>
2022-02-14 16:07:53 -0800

    edit FeaturedPlants path

---
+++ frontend/Components/homepage/FeaturedPlants.tsx
@@ -0,0 +1,33 @@
+import SectionWrapper from "../../hoc/Layout/SectionWrapper";
+import SectionTitle from "../common/SectionTitle";
+import ProductCard from "../common/ProductCard";
+import EmblaCarouselComponent from "../emblaCarousel/EmblaCarousel";
+
+const FeaturedPlants = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Featured Plants</SectionTitle>
+      <EmblaCarouselComponent>
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-2.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-3.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-4.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-5.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+        <ProductCard productName="Crassula Ovata" imgUrl="/img/plant-1b.png" />
+      </EmblaCarouselComponent>
+    </SectionWrapper>
+  );
+};
+
+export default FeaturedPlants;

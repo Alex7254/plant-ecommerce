a7c1157e136649a307b1da764ab754819e45ed98
Alejandro <alejandro199898@gmail.com>
2022-02-14 16:12:08 -0800

    create file for about section

---
+++ frontend/Components/homepage/About.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import SectionTitle from "../common/SectionTitle";
+
+const FeaturedPlants = () => {
+  return <SectionTitle>About</SectionTitle>;
+};
+
+export default FeaturedPlants;

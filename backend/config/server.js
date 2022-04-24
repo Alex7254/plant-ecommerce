bdf302cd0b56fcc7a6b27f56a8b519e9181742c2
Alejandro <alejandro199898@gmail.com>
2022-04-23 19:42:01 -0700

    remove backend folder to re-do using golang

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '3dee7a75122162be1cbfaeefcf5b27c9'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '795f8ea0-edba-4bfe-9776-131e71907b58'
-};

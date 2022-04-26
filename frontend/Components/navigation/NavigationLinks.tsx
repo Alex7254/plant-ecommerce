276760204c11ed4453d444d3d9aa59dc3fd5e2f2
Alejandro <alejandro199898@gmail.com>
2022-04-26 14:26:45 -0700

    edit nav layout

--- frontend/Components/navigation/NavigationLinks.tsx
+++ frontend/Components/navigation/NavigationLinks.tsx
@@ -4,38 +4,30 @@ import NavigationLink from "./NavigationLink";
 import CartButton from "./CartButton";
 import CactusLogo from "./Logo";
 
+const StyledNav = styled.nav`
+  background-color: var(--beige);
+  margin: 0 auto;
+  z-index: 2;
+  position: fixed;
+  left: 0;
+  right: 0;
+`;
+
 const StyledUl = styled.ul`
+  max-width: 90rem;
+  padding: 0.5rem 4rem;
+  margin: 0 auto;
   list-style-type: none;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  width: 120rem;
-  max-width: 120rem;
-  margin: 0 auto;
   > div {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
   }
 `;
-
-const StyledNav = styled.nav`
-  background-color: var(--beige);
-  width: 100%;
-  margin: 0 auto;
-  z-index: 2;
-  position: fixed;
-  left: 0;
-  right: 0;
-  padding: 0.5rem 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const cartButton = styled.a;
 
 const navigationItems = () => (
   <StyledNav>
